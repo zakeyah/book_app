d313017a7358020cddecef4465aab8fc29d4bbb7
zakeyah <mom11omar@gmail.com>
2021-04-04 17:21:56 +0300

    first

---
+++ server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+const superagent = require('superagent');
+
+
+
+const app = express();
+const PORT = process.env.PORT || 3000 ;
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+
+app.get('/',(req,res)=> res.render('pages/index'));
+
+
+
+
